e04c7887bf940c4773fc8955b6d5a7952b834376
Mr-Inferno-01 <aditya9984xz@gmail.com>
2026-08-08 10:21:39 +0530

    Time: 1 ms (26.60%) | Memory: 19.2 MB (94.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,14 +3,13 @@ class Solution:
         if not s:
             return ""
 
-
-
-        for i in range(len(s[0])):
-            char = s[0][i]
-
-            for j in range(1,len(s)):
-                if i == len (s[j]) or s[j][i] != char:
-                    return s[0][:i]
-
         
+        for  i in range(len(s[0])):
+            char = s[0][i]
+                       
+            for j in range(1,len(s)):
+                if i == len(s[j]) or s[j][i] != char:
+
+                    return s[0][:i]
+            
         return s[0]
