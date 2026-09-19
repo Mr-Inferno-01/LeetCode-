2a24feea0cc832d2f1ddf1b8b6a5b977d9119a2d
Mr-Inferno-01 <aditya9984xz@gmail.com>
2026-09-19 18:21:36 +0530

    Time: 1 ms (28.44%) | Memory: 19.3 MB (32.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,10 +4,10 @@ class Solution:
             return ""
 
 
-        for i in range(len (s[0])):
+        for i in range(len(s[0])):
             char = s[0][i]
 
-            for j in range(1, len(s)):
+            for j in range (1,len(s)):
                 if i == len(s[j]) or s[j][i] != char:
                     return s[0][:i]
 
